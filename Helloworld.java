fd2842d5d3c94a81a509f00e2c59df1ed6638b8b
Atul9119 <104543807+Atul9119@users.noreply.github.com>
2022-05-11 12:39:37 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,7 @@
 public class Helloworld{
   public static void main(String[] args){
     System.out.println("Git demo");
+    System.out.println("hi this is for github and jenkins example");
+    
 }
 }
